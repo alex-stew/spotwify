6c030adaee6a2cc4540efbd0ba7e72ccfbd3c3e6
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-01-28 21:46:40 +1030

    alex

--- asset/main.js
+++ asset/main.js
@@ -5,13 +5,14 @@ let currentAlbumEl = document.querySelector("#currentAlbum");
 let navigation = document.querySelector('.navigation');
 let toggle = document.querySelector('.toggle');
 let content = document.querySelector('.content-wrapper');
-let sidebarFull = '<div class="sidebar-menu" style="max-width:86%;height:auto;"><a href="#" class="sidebar-brand"><img id="frontLogo" src="./asset/logo.png">SPOTWIFY</a><div class="sidebar-content"><span><i class="fa fa-search" aria-hidden="true"></i></span><input type="text" class="form-control" style="z-index: 10;" placeholder="Artist" /></div><a href="#twitter"><i class="fa fa-twitter" aria-hidden="true"></i><span class="sidebar-link" id="navLink1">TWITTER</span></a><br /><div class="sidebar-divider"></div><a href="#artistInfoSection"><span class="icon"><i class="fa fa-info-circle" aria-hidden="true"></i></span><span class="sidebar-link" id="navLink2">ARTIST INFO</span></a><br /><div class="sidebar-divider"></div><a href="#album"><span class="icon"><i class="fa fa-circle-o" aria-hidden="true"></i></span><span class="sidebar-link" id="navLink3">ALBUM</span></a><br/><div class="sidebar-divider"></div></div>';
+let sidebarFull = '<div class="sidebar-menu" style="max-width:86%;height:auto;"><a href="#" class="sidebar-brand"><img id="frontLogo" src="./asset/logo.png">SPOTWIFY</a><div class="sidebar-content"><span><i class="fa fa-search" aria-hidden="true"></i></span><input id="spotSearch" type="text" class="form-control" style="z-index: 10;" placeholder="Artist" /></div><a href="#twitter"><i class="fa fa-twitter" aria-hidden="true"></i><span class="sidebar-link" id="navLink1">TWITTER</span></a><br /><div class="sidebar-divider"></div><a href="#artistInfoSection"><span class="icon"><i class="fa fa-info-circle" aria-hidden="true"></i></span><span class="sidebar-link" id="navLink2">ARTIST INFO</span></a><br /><div class="sidebar-divider"></div><a href="#album"><span class="icon"><i class="fa fa-circle-o" aria-hidden="true"></i></span><span class="sidebar-link" id="navLink3">ALBUM</span></a><br/><div class="sidebar-divider"></div></div>';
 let sidebarEmpty = '<div class="sidebar-menu" style="max-width:86%;height:auto;"><a href="#" class="sidebar-brand"><img id="frontLogo" src="./asset/logo.png"></a><br /><div class="sidebar-divider"></div></div>';
 let audioEl = document.querySelector('audio');
 
 let navigationActive = false;
 var videoEle = document.getElementById("video");
 var songs = [];
+
 function togglemenu() {
   navigationActive = !navigationActive;
 
@@ -21,26 +22,26 @@ function togglemenu() {
     navigation.innerHTML = "";
     navigation.innerHTML = sidebarEmpty;
 
-    if(navigation.classList.contains("active"))
-      navigation.classList.remove("active");  
+    if (navigation.classList.contains("active"))
+      navigation.classList.remove("active");
   }
   else {
     content.style.marginLeft = '300px';
     content.style.width = '80%';
     navigation.innerHTML = "";
-    navigation.innerHTML = sidebarFull;   
+    navigation.innerHTML = sidebarFull;
 
-    if(!navigation.classList.contains("active"))
+    if (!navigation.classList.contains("active"))
       navigation.classList.add("active");
   }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   content.style.marginLeft = '60px';
   content.style.width = '96%';
   navigation.innerHTML = "";
   navigation.innerHTML = sidebarEmpty;
-  
+
   class Song {
     constructor(vid, image, songNmae, description) {
       this.vid = vid;
@@ -85,7 +86,7 @@ $(document).ready(function() {
       tr.append(tdImage);
       tr.append(tdName);
       tr.on("click", play);
-      tr.on("click",songInfo);
+      tr.on("click", songInfo);
       $("#songList").append(tr);
     }
   }
@@ -110,35 +111,35 @@ $(document).ready(function() {
     var popup = $("#popup");
     var index = jQuery(this).children("th").text();
     console.log(songs[index].description);
-    if(songs[index].description === ""){
+    if (songs[index].description === "") {
       popup.text("NUll");
-    }else{
+    } else {
       popup.text(songs[index].description);
     }
-    
+
     $("#description").show();
-    
+
   }
 
-  if(navigation.classList.contains("active"))
-    navigation.classList.remove("active");  
+  if (navigation.classList.contains("active"))
+    navigation.classList.remove("active");
 
   navigation.addEventListener("mouseenter", function (event) {
     event.stopPropagation();
 
-    if(!navigation.classList.contains("active")) {
+    if (!navigation.classList.contains("active")) {
       navigationActive = true;
       content.style.marginLeft = '300px';
 
       content.style.width = '80%';
-      navigation.innerHTML = sidebarFull;  
+      navigation.innerHTML = sidebarFull;
     }
   });
 
   navigation.addEventListener("mouseleave", function (event) {
     event.stopPropagation();
 
-    if(!navigation.classList.contains("active")) {
+    if (!navigation.classList.contains("active")) {
       navigationActive = false;
       content.style.marginLeft = '60px';
 
@@ -152,121 +153,116 @@ $(document).ready(function() {
   var infoURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + query;
 
   $.ajax({
-      url:infoURL,
-      method: "GET"
-  }).then(function(infoRes){
-      console.log(infoRes);
-      displayArtistResults(infoRes);
-  }); 
+    url: infoURL,
+    method: "GET"
+  }).then(function (infoRes) {
+    console.log(infoRes);
+    displayArtistResults(infoRes);
+  });
 
   function displayArtistResults(infoRes) {
-      artistNameEl.text(infoRes.artists[0].strArtist);
-      $(artistInfoEl).text(infoRes.artists[0].strBiographyEN);
-      id = infoRes.artists[0].idArtist;
-      console.log(id);
-      getSongs(id);
+    artistNameEl.text(infoRes.artists[0].strArtist);
+    $(artistInfoEl).text(infoRes.artists[0].strBiographyEN);
+    id = infoRes.artists[0].idArtist;
+    console.log(id);
+    getSongs(id);
   }
 
 
-  
+
   //TWITTER AJAX CALL
   const bearerToken = 'AAAAAAAAAAAAAAAAAAAAAPSsLwEAAAAAfmGkC5w40GXnulvQILRwRWbNnH8%3DWZ6GrB5H83CPvjFLyBfTzKvdTXFgHaikXFyesyHeJ4yTkaEWpD';
   var twitterHandle = "Tool";
+  $.ajax({
+    url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/by/username/" + twitterHandle,
+    method: "GET",
+    timeout: 0,
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
+    },
+    success: function (response) {
+      if (response.data != undefined) {
+        $('#twitter').html('');
+        getTweets(response.data.id);
+        getMentions(response.data.id);
+      } else {
+        console.log("Could not find user!");
+      }
+    }
+  });
+
+  var getTweets = function (id) {
     $.ajax({
-      url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/by/username/" + twitterHandle,
+      url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/" + id + "/tweets",
       method: "GET",
       timeout: 0,
+      data: "expansions=author_id",
       beforeSend: function (xhr) {
         xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
       },
-      success: function(response)
-      {
-        if(response.data != undefined) {
-          $('#twitter').html('');
-          getTweets(response.data.id);
-          getMentions(response.data.id);
-        } else {
-          console.log("Could not find user!");
+      success: function (response) {
+        if (response.data != undefined) {
+          $('#twitter').append('<h3>TWEETS: </h3><br />');
+          var ul = $("<ul>");
+
+          var index = 0;
+
+          response.data.forEach(function (data) {
+            if (++index < 5) {
+              $.ajax({
+                url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/" + data.author_id,
+                method: "GET",
+                timeout: 0,
+
+                beforeSend: function (xhr) {
+                  xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
+                },
+                success: function (response) {
+                  ul.append("<p style='padding: 20px; border-radius: 5px; background-color: aliceblue; margin-bottom: 5px;'>" + data.text + "</p><h4 style='margin-bottom: 25px;'>" + response.data.username + "</h4>");
+                }
+              });
+            }
+          });
+          ul.appendTo($('#twitter'));
         }
       }
     });
+  }
 
-    var getTweets = function(id) {
-      $.ajax({
-        url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/"+id+"/tweets",
-        method: "GET",
-        timeout: 0,
-        data: "expansions=author_id",
-        beforeSend: function (xhr) {
-          xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
-        },
-        success: function(response)
-        {
-          if(response.data != undefined) {
-            $('#twitter').append('<h3>TWEETS: </h3><br />');
-            var ul = $("<ul>");
+  var getMentions = function (id) {
+    $.ajax({
+      url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/" + id + "/mentions",
+      method: "GET",
+      timeout: 0,
+      data: 'expansions=author_id',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
+      },
+      success: function (response) {
+        if (response.data != undefined) {
+          var ul = $("<ul>");
+          $('#twitter').append('<h3>MENTIONS: </h3><br />');
 
-            var index = 0;
+          var index = 0;
 
-            response.data.forEach(function(data) {
-              if(++index < 5) {
-                $.ajax({
-                  url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/"+data.author_id,
-                  method: "GET",
-                  timeout: 0,
-          
-                  beforeSend: function (xhr) {
-                    xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
-                  },
-                  success: function(response)
-                  {
-                    ul.append("<p style='padding: 20px; border-radius: 5px; background-color: aliceblue; margin-bottom: 5px;'>"+ data.text +"</p><h4 style='margin-bottom: 25px;'>"+ response.data.username+"</h4>");
-                  }
-                });
-              }
-            });
+          response.data.forEach(function (data) {
+            if (++index < 5)
+              $.ajax({
+                url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/" + data.author_id,
+                method: "GET",
+                timeout: 0,
+
+                beforeSend: function (xhr) {
+                  xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
+                },
+                success: function (response) {
+                  ul.append("<p style='padding: 20px; border-radius: 5px; background-color: aliceblue; margin-bottom: 5px;'>" + data.text + "</p><h4 style='margin-bottom: 25px;'>" + response.data.username + "</h4>");
+                }
+              });
+          });
           ul.appendTo($('#twitter'));
-          }
         }
-      });
-    }
-
-    var getMentions = function(id) {
-      $.ajax({
-        url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/"+id+"/mentions",
-        method: "GET",
-        timeout: 0,
-        data: 'expansions=author_id',
-        beforeSend: function (xhr) {
-          xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
-        },
-        success: function(response)
-        {
-          if(response.data != undefined) {
-            var ul = $("<ul>");
-            $('#twitter').append('<h3>MENTIONS: </h3><br />');
-
-            var index = 0;
-
-            response.data.forEach(function(data) {
-              if(++index < 5)
-                $.ajax({
-                  url: "http://cors-anywhere.herokuapp.com/https://api.twitter.com/2/users/"+data.author_id,
-                  method: "GET",
-                  timeout: 0,
-          
-                  beforeSend: function (xhr) {
-                    xhr.setRequestHeader('Authorization', 'bearer ' + bearerToken);
-                  },
-                  success: function(response)
-                  {
-                    ul.append("<p style='padding: 20px; border-radius: 5px; background-color: aliceblue; margin-bottom: 5px;'>"+ data.text +"</p><h4 style='margin-bottom: 25px;'>"+ response.data.username+"</h4>");
-                  }
-                });            
-            });
-            ul.appendTo($('#twitter'));
-          }
-        }
-      });
-    }
-  });
+      }
+    });
+  }
+});
