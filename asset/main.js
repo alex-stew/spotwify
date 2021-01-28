e5828acb10bc4c70f276e07bca4144e7a3e1eb16
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-01-28 16:50:28 +1030

    alex wip

--- asset/main.js
+++ asset/main.js
@@ -2,8 +2,6 @@ let artistNameEl = document.querySelector("#artistName");
 let artistInfoEl = document.querySelector("#artistInfo");
 let albumNameEl = document.querySelector("#albumTitle");
 let currentAlbumEl = document.querySelector("#currentAlbum");
-
-
 let navigation = document.querySelector('.navigation');
 let toggle = document.querySelector('.toggle');
 let content = document.querySelector('.content-wrapper');
@@ -11,50 +9,50 @@ let content = document.querySelector('.content-wrapper');
 // let sidebarEmpty = '<div class="sidebar-menu" style="max-width:86%;height:auto;"><ul><a href="#" class="sidebar-brand"><img id="frontLogo" src="./asset/logo.png"></a></li><div class="sidebar-content"><input type="text" class="form-control"></div><li><a href="#twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li><br /><div class="sidebar-divider"></div><li><a href="#artistInfoSection"><span class="icon"><i class="fa fa-info-circle" aria-hidden="true"></i></span></a></li><br /><div class="sidebar-divider"></div><li><a href="#albrum"><span class="icon"><i class="fa fa-circle-o" aria-hidden="true"></i></span></a></li><br /><div class="sidebar-divider"></div><li><audio controls id="music"><span><i class="fa fa-play-circle-o" aria-hidden="true"></i></span><source src="https://open.spotify.com/track/0E4Y1XIbs8GrAT1YqVy6dq" type="audio/ogg"></audio></li></ul></div>';
 let audioEl = document.querySelector('audio')
 
-function togglemenu() {
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
-    if (!navigation.classList.contains('active')) {
-        content.style.marginLeft = '60px';
-        content.style.width = '96%';
-        navigation.innerHTML = "";
-        navigation.innerHTML = sidebarEmpty;
-        audioEl.setAttribute('class', "active");
-    }
-    else {
-        content.style.marginLeft = '300px';
-        content.style.width = '80%';
-        navigation.innerHTML = "";
-        navigation.innerHTML = sidebarFull;                
-        audioEl.setAttribute('class', "hide");
-    }
-    media()
-}
+// function togglemenu() {
+//     navigation.classList.toggle('active');
+//     toggle.classList.toggle('active');
+//     if (!navigation.classList.contains('active')) {
+//         content.style.marginLeft = '60px';
+//         content.style.width = '96%';
+//         navigation.innerHTML = "";
+//         navigation.innerHTML = sidebarEmpty;
+//         audioEl.setAttribute('class', "active");
+//     }
+//     else {
+//         content.style.marginLeft = '300px';
+//         content.style.width = '80%';
+//         navigation.innerHTML = "";
+//         navigation.innerHTML = sidebarFull;                
+//         audioEl.setAttribute('class', "hide");
+//     }
+//     media()
+// }
 
-navigation.addEventListener("mouseenter", function (event) {
-    content.style.marginLeft = '300px';
-    content.style.width = '80%';
-    navigation.innerHTML = "";
-    navigation.innerHTML = sidebarFull;
-    media()
-})
+// navigation.addEventListener("mouseenter", function (event) {
+//     content.style.marginLeft = '300px';
+//     content.style.width = '80%';
+//     navigation.innerHTML = "";
+//     navigation.innerHTML = sidebarFull;
+//     media()
+// })
 
-navigation.addEventListener("mouseleave", function () {
-    content.style.marginLeft = '60px';
-    content.style.width = '96%';
-    navigation.innerHTML = "";
-    navigation.innerHTML = sidebarEmpty;
-    media()
-});
+// navigation.addEventListener("mouseleave", function () {
+//     content.style.marginLeft = '60px';
+//     content.style.width = '96%';
+//     navigation.innerHTML = "";
+//     navigation.innerHTML = sidebarEmpty;
+//     media()
+// });
 
-function media(){
-    if(document.querySelector('body').style.width<400){
-        content.style.margin = 0;
-    }
-}
+// function media(){
+//     if(document.querySelector('body').style.width<400){
+//         content.style.margin = 0;
+//     }
+// }
 
 //ARTIST INFO AJAX REQUEST
-
+var id = "";
 var query = "tool";
 var infoURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" + query;
 
@@ -63,14 +61,16 @@ $.ajax({
     method: "GET"
 }).then(function(infoRes){
     console.log(infoRes);
+    displayArtistResults(infoRes);
 }); 
 
 function displayArtistResults(infoRes) {
-    $(artistNameEl).val(JSON.stringify(infoRes.artists[0].strArtist));
-    $(artistInfoEl).val(JSON.stringify(infoRes.artists[0].strBiographyEN));
+    $(artistNameEl).text(JSON.stringify(infoRes.artists[0].strArtist));
+    $(artistInfoEl).text(JSON.stringify(infoRes.artists[0].strBiographyEN));
+    id = infoRes.artists[0].idArtist;
+    console.log(id);
 }
 
-displayArtistResults();
 
 //YOUTUBE AJAX CALL
 var videoEle = document.getElementById("video");
